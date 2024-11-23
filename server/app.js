e4c832329cb348a7a0d263d28bd0dd383cf900fa
Beprodeep Das <codingbtech@gmail.com>
2024-11-23 13:41:42 +0530

    initializing

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+// Create Express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
